a62ca04e4f203f185a2596609114d826d3603b31
Dennis Dunn <ansofive@gmail.com>
2022-03-05 10:59:25 -0700

    Reorganization. Again.

--- chaos.py
+++ chaos.py
@@ -1,22 +1,13 @@
 import urandom
+from misc import randxy, randcolor
 
 def init(driver):
     width =  driver.get_width()
     height = driver.get_height()
 
     def fsm(state):
-        
-        def rcell():
-            return (urandom.randint(0, width), urandom.randint(0, height))
-
-        def rcolor():
-            if urandom.random() < 0.75:
-                return (urandom.randint(0, 255), urandom.randint(0, 255), urandom.randint(0, 255))
-            else:
-                return (0, 0, 0)
-
         next = {}
-        next[rcell()] = {'color': rcolor()}
+        next[randxy((width, height))] = {'color': randcolor()}
         return next
 
     return fsm
